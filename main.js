3b16d74132b81f4cb5bb7378913378cc3a37a559
Eugene <deus.sles@gmail.com>
2018-11-21 19:22:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.slider-slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img src="img/slideleft.png" width="60px" class="slider-left">',
+    nextArrow: '<img src="img/slideright.png" width="60px" class="slider-right">'
+  });
+          
